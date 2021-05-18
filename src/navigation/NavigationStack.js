5c7173fcd59ff214e290783b9128a02b6acdf7d7
Firman Manalu <59146810+man14116086@users.noreply.github.com>
2021-05-19 03:20:03 +0700

    Update

---
+++ src/navigation/NavigationStack.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { enableScreens } from 'react-native-screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
+import { Host } from 'react-native-portalize';
+
+import {
+  MoviesStackScreen,
+  SearchStackScreen
+} from './screens';
+import { ROUTES, TABS } from './routes';
+
+import { darkBlue, white, pink, blue } from '../utils/colors';
+
+enableScreens();
+
+const defaultNavigationOptions = {
+  headerTintColor: darkBlue,
+  headerStyle: {
+    backgroundColor: white
+  }
+};
+
+const Tab = createBottomTabNavigator();
+const TabsConfig = {
+  tabBarOptions: {
+    activeTintColor: pink,
+    inactiveTintColor: blue,
+    labelStyle: {
+      margin: 0,
+      padding: 1
+    },
+    style: {
+      backgroundColor: white
+    }
+  }
+};
+
+const AppNavigator = () => (
+  <NavigationContainer>
+    <Host>
+      <Tab.Navigator initialRouteName={ROUTES.MOVIE_LIST} {...TabsConfig}>
+        <Tab.Screen
+          name={TABS.HOME}
+          component={MoviesStackScreen}
+          options={{
+            ...defaultNavigationOptions,
+            tabBarIcon: ({ color }) => (
+              <Feather name="NKuy" size={20} color={color} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name={TABS.SEARCH}
+          component={SearchStackScreen}
+          options={{
+            ...defaultNavigationOptions,
+            tabBarIcon: ({ color }) => (
+              <Feather name="Pencarian" size={20} color={color} />
+            )
+          }}
+        />
+      </Tab.Navigator>
+    </Host>
+  </NavigationContainer>
+);
+
+export default AppNavigator;
---
+++ src/navigation/routes.js
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  MOVIE_LIST: 'List Movie',
+  MOVIE_DETAILS: 'Detail Movie',
+  SEARCH: 'Pencarian',
+  SEARCH_RESULTS: 'Hasil Pencarian'
+};
+
+export const TABS = {
+  HOME: 'NKuy',
+  SEARCH: 'Cari'
+};
